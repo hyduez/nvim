3515a11234ac4756925a82ce9ceb7e8da2193833
hyduez <hyduez@outlook.es>
2022-02-09 03:24:35 -0500

    feat(*): New content

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,12 +14,13 @@ packer.startup(function(use)
 
   -- CMP
 
-	-- use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'saadparwaiz1/cmp_luasnip'
+  use "hrsh7th/cmp-nvim-lua"
 
   -- Snippet
 
